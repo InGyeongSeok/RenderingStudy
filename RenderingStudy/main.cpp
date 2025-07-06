5b495198cf19a11b20af4f6e04131a1e49982ccd
InGyeong <dev.ingyeong@gmail.com>
2025-07-06 22:46:11 +0900

    Device & Command Queue

--- RenderingStudy/main.cpp
+++ RenderingStudy/main.cpp
@@ -5,10 +5,16 @@
 
 #include <Debug/DXDebugLayer.h>
 
+#include <D3D/DXContext.h>
+
 int main()
 {
 	DXDebugLayer::Get().Init();
 
+	if (DXContext::Get().Init())
+	{
+		DXContext::Get().Shutdown();
+	}
 	DXDebugLayer::Get().Shutdown();
 	
 }
